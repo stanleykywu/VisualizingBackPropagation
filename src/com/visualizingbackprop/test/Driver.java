9d0a3d603f0da1b96e5bc8e6cd08b5987b9c2d1c
stanleykywu <stanleykywu@gmail.com>
2020-04-24 15:30:35 -0400

    Added random data generation

--- src/com/visualizingbackprop/test/Driver.java
+++ src/com/visualizingbackprop/test/Driver.java
@@ -2,21 +2,56 @@ package com.visualizingbackprop.test;
 
 import tester.Tester;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Driver {
     public static void main(String ...args) {
-        double[][] out = new double[2][1];
-        double[][] in = new double[1][1];
+        ArrayList<double[][]> inputs = generateRandomPoints(100);
+        ArrayList<double[][]> expOut = generateCorrespondingOutput(inputs);
 
-        out[0][0] = 1;
-        out[1][0] = 0;
-        in[0][0] = 0.3;
+        Net simple = new Net(inputs, expOut, true);
 
-        Net simple = new Net(out, true);
-
-//        for(int epoch = 0; epoch < 100; epoch++) {
-//            System.out.println(simple.propAndUpdate(in));
+//        for(int epoch = 0; epoch < 1000; epoch++) {
+//            System.out.println(simple.propAndUpdate());
 //        }
-        WorldVisual yay = new WorldVisual(simple, in);
-        yay.bigBang(500, 500, 2);
+
+        WorldVisual simpleVisual = new WorldVisual(simple);
+        simpleVisual.bigBang(500, 500, 0.5);
+    }
+
+    public static ArrayList<double[][]> generateRandomPoints(int numPoints) {
+        ArrayList<double[][]> result = new ArrayList<double[][]>();
+        for (int i = 0; i < numPoints; i++) {
+            double[][] point = new double[2][1];
+            int xRange = 200;
+            int yRange = 200;
+
+            point[0][0] = (double) (new Random().nextInt(xRange) - 100) / 100;
+            point[1][0] = (double) (new Random().nextInt(yRange) - 100) / 100;
+
+            result.add(point);
+//            System.out.println(point[0][0] + " " + point[1][0]);
+        }
+        return result;
+    }
+
+    public static ArrayList<double[][]> generateCorrespondingOutput(ArrayList<double[][]> input) {
+        ArrayList<double[][]> result = new ArrayList<double[][]>();
+
+        for(double[][] point : input) {
+            double[][] resultPoint = new double[2][1];
+            double x = point[0][0] * 100;
+            double y = point[1][0] * 100;
+            if (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) <= 50) {
+                resultPoint[0][0] = 1;
+                resultPoint[1][0] = 0;
+            } else {
+                resultPoint[0][0] = 0;
+                resultPoint[1][0] = 1;
+            }
+            result.add(resultPoint);
+        }
+        return result;
     }
 }
