89d4e80fee63f19c15002f1d49c877cec2c3a608
stanleykywu <stanleykywu@gmail.com>
2020-04-26 16:38:03 -0400

    Model Function y > x^2

--- src/com/visualizingbackprop/test/Driver.java
+++ src/com/visualizingbackprop/test/Driver.java
@@ -5,24 +5,24 @@ import java.util.Random;
 
 public class Driver {
     public static void main(String ...args) {
-        ArrayList<double[][]> inputs = generateRandomPoints(50);
+        ArrayList<double[][]> inputs = generateRandomPoints(2500);
         ArrayList<double[][]> expOut = generateCorrespondingOutput(inputs);
 
         Net simple = new Net(inputs, expOut, true);
 
         WorldVisual simpleVisual = new WorldVisual(simple);
-        simpleVisual.bigBang(500, 600, 2);
+        simpleVisual.bigBang(500, 600, 0.2);
     }
 
     public static ArrayList<double[][]> generateRandomPoints(int numPoints) {
         ArrayList<double[][]> result = new ArrayList<double[][]>();
         for (int i = 0; i < numPoints; i++) {
             double[][] point = new double[2][1];
-            int xRange = 200;
-            int yRange = 200;
+            int xRange = 100;
+            int yRange = 100;
 
-            point[0][0] = (double) (new Random().nextInt(xRange) - 100) / 100;
-            point[1][0] = (double) (new Random().nextInt(yRange) - 100) / 100;
+            point[0][0] = (double) (new Random().nextInt(xRange) - 50) / 50;
+            point[1][0] = (double) (new Random().nextInt(yRange) - 50) / 50;
 
             result.add(point);
         }
@@ -36,13 +36,14 @@ public class Driver {
             double[][] resultPoint = new double[2][1];
             double x = point[0][0] * 100;
             double y = point[1][0] * 100;
-            if (y > 0.1 * Math.pow(x, 2)) {
+            if (y > Math.pow(x, 2)) {
                 resultPoint[0][0] = 1;
-                resultPoint[1][0] = 0;
             } else {
-                resultPoint[0][0] = 0;
-                resultPoint[1][0] = 1;
+                resultPoint[0][0] = 0.5;
             }
+//            System.out.println(point[0][0] + " " + point[1][0]);
+//            System.out.println(resultPoint[0][0]);
+//            System.out.println();
             result.add(resultPoint);
         }
         return result;
